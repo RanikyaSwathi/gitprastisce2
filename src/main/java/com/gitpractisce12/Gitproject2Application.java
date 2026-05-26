feb52ea1324c47c6787f4c562f1f0f9eff48ddc4
RanikyaSwathi <swathi.r@softsuave.org>
2026-05-26 19:22:56 +0530

    initial project commit

---
+++ src/main/java/com/gitpractisce12/Gitproject2Application.java
@@ -0,0 +1,13 @@
+package com.gitpractisce12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpractisce12/Gitproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpractisce12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
